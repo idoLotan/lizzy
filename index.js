3378ecc83e770d1274ee4ad3c607e577431712e2
idoLotan <91139751+idoLotan@users.noreply.github.com>
2023-06-07 20:33:14 +0300

    js done

--- index.js
+++ index.js
@@ -14,12 +14,21 @@ svg.forEach((button) => {
   });
 });
 
-const extand = document.getElementsByName("open-btn");
+const extandBtn = document.getElementsByName("extand")[0];
+const shrinkBtn = document.getElementsByName("shrink")[0];
 const toolBar = document.getElementById("tool-bar");
+
 const arrowIcon = document.getElementById("arrow-icon");
-extand.addEventListener("click", function (event) {
+extandBtn.addEventListener("click", function (event) {
   toolBar.classList.remove("w-72");
   toolBar.classList.add("max-w-4xl"); // Remove the background color class
   arrowIcon.classList.remove("fill-[#ABABAC]");
   arrowIcon.classList.add("fill-[#25A7FF]");
+});
+
+shrinkBtn.addEventListener("click", function (event) {
+  toolBar.classList.remove("max-w-4xl");
+  toolBar.classList.add("w-72"); // Remove the background color class
+  arrowIcon.classList.remove("fill-[#25A7FF]");
+  arrowIcon.classList.add("fill-[#ABABAC]");
 });
